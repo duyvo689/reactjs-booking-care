3f8fbaf59064e1feb12e3c3540d4405ad2ea5442
duyvo689 <vothanhduy689@gmail.com>
2021-09-24 03:27:54 +0700

    header web

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,58 +1,61 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { NavLink } from 'reactstrap';
-import './HomeHeader.scss'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "reactstrap";
+import "./HomeHeader.scss";
+import "../../containers/Base.scss"
 
 class HomeHeader extends Component {
-
-    render() {
-        return (
-            <>
-            <div className="home-header-container">
-                <div className="home-header-content">
-                    <div className="left-content">
-                        <i className="fas fa-bars"></i>
-                        <div className="header-logo"></div>
-                    </div>
-                    <div className="center-content">
-                        <ul className="center-child">
-                            <li className="center-child-item">
-                                <NavLink exact activeClassName="active" to="/">Chuyên khoa</NavLink>
-                            </li>
-                            <li className="center-child-item">
-                                <NavLink exact activeClassName="active" to="/">Bác sĩ</NavLink>
-                            </li>
-                            <li className="center-child-item">
-                                <NavLink exact activeClassName="active" to="/">Phòng khám</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="right-content">
-                        <div className="right-child">
-                            <i className="fas fa-question-circle"></i>
-                            <span>Hỗ trợ</span>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <>
+        <div className="home-header-container">
+          <div className="home-header-content grid">
+            <div className="home-left-content col c-3">
+              <div className="home-logo"></div>
             </div>
-            <div className="home-header-banner">
+            <div className="home-right-content col c-9">
+              <div className="home-right-content-cover">
+                <form className="home-search">
+                  <input type="text" name="q" class="home-search-input"
+                    id="header-search-input"
+                    placeholder="Search for health information"
+                  />
+                  <button className="home-search-btn">Go</button>
+                </form>
+
+                <ul className="home-item">
+                  <li className="home-item-select">
+                    <NavLink exact='true' activeClassName="active" to="/">Chuyên Khoa</NavLink>
+                  </li>
+                  <li className="home-item-select">
+                    <NavLink exact='true' activeClassName="active" to="/">Chuyên Khoa</NavLink>
+                  </li>
+                  <li className="home-item-select">
+                    <NavLink exact='true' activeClassName="active" to="/">Chuyên Khoa</NavLink>
+                  </li>
+                  <li className="home-item-select">
+                    <NavLink exact='true' activeClassName="active" to="/">Chuyên Khoa</NavLink>
+                  </li>
+                </ul>
+              </div>
 
             </div>
-            </>
-        );
-    }
-
+          </div>
+        </div>
+        <div className="home-header-banner"></div>
+      </>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.admin.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.admin.isLoggedIn,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
