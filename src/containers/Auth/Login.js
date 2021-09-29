ada3439893eae36646c549e847c9e576590ea8c1
duyvo689 <vothanhduy689@gmail.com>
2021-09-30 02:17:04 +0700

    set event for form login

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -11,6 +11,37 @@ import { FormattedMessage } from 'react-intl';
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: 'duy vo',
+            password: '123456',
+            isShowPassword: false,
+        }
+    }
+
+    handleOnChangeEmail = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    handleOnChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    handleOnClick = () => {
+        console.log(this.state.username)
+        console.log(this.state.password)
+
+    }
+
+    handleShowHidePassword = () => {
+        this.setState({
+            isShowPassword: !this.state.isShowPassword
+        })
     }
 
     render() {
@@ -19,15 +50,33 @@ class Login extends Component {
             <div className='login-backgroud'>
                 <div className='login-container'>
                     <div className='login-content'>
-                        <form className='login-form'>
+                        <div className='login-form'>
                             <div className="login-title">Login</div>
                             <label>Username</label>
-                            <input description="Username" placeholder="Enter your username" type="text" />
+                            <input
+                                description="Username"
+                                placeholder="Enter your username" type="text"
+                                value={this.state.username}
+                                onChange={(event) => this.handleOnChangeEmail(event)}
+                            />
                             <label>Password</label>
-                            <input description="Password" placeholder="Enter your password" type="password" />
-                            <button>Login</button>
-                        </form>
-                        <div className='login-with'>Or sing in with</div>
+                            <div className='custom-input-password'>
+                                <input
+                                    description="Password"
+                                    placeholder="Enter your password"
+                                    type={this.state.isShowPassword ? 'text' : 'password'}
+                                    value={this.state.password}
+                                    onChange={(event) => this.handleOnChangePassword(event)}
+                                />
+                                <span onClick={() => this.handleShowHidePassword()}>
+                                    <i className={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
+                                </span>
+                            </div>
+                            <button type="submit" onClick={() => this.handleOnClick()}>Login</button>
+                        </div>
+                        <div className='login-forgot-password'>Forgot password?</div>
+
+                        <div className='login-with'>Or sign in with</div>
                         <div className='login-social'>
                             <i className="facebook-icon fab fa-facebook"></i>
                             <i className="twitter-icon fab fa-twitter"></i>
