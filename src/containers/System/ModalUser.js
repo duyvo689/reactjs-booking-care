2038efc48a9a5028f7925822ce34f059c39121dd
duyvo689 <vothanhduy689@gmail.com>
2021-10-01 23:45:44 +0700

    design modal create  user

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -1,5 +1,4 @@
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 class ProductManage extends Component {
@@ -19,20 +18,50 @@ class ProductManage extends Component {
     render() {
         return (
             < div >
-                <Modal isOpen={this.props.isOpen} toggle={() => this.toggle()} className={''}>
-                    <ModalHeader toggle={() => this.toggle()}>Modal title</ModalHeader>
+                <Modal isOpen={this.props.isOpen}
+                    toggle={() => this.toggle()}
+                    className={'modal-user-container'}
+                    size="lg"
+                    centered
+                >
+                    <ModalHeader toggle={() => this.toggle()}>Add new user</ModalHeader>
                     <ModalBody>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <div className={"form-input"}>
+                            <div className="input-cover">
+                                <div className="input-1">
+                                    <label>Email</label>
+                                    <input type='email' placeholder='Nhap email' />
+                                </div>
+
+                                <div className="input-1">
+                                    <label>Password</label>
+                                    <input type='password' placeholder='Nhap password' />
+                                </div>
+                            </div>
+                            <div className="input-cover">
+                                <div className="input-1">
+                                    <label>First Name</label>
+                                    <input type='text' placeholder='Nhap first name' />
+                                </div>
+                                <div className="input-1">
+                                    <label>Last Name</label>
+                                    <input type='text' placeholder='Nhap last name' />
+                                </div>
+                            </div>
+                            <div className="input-2">
+                                <label>Address</label>
+                                <input type='text' placeholder='Nhap address' />
+                            </div>
+                        </div>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="primary" onClick={() => this.toggle()}>Do Something</Button>{' '}
-                        <Button color="secondary" onClick={() => this.toggle()}>Cancel</Button>
+                        <Button className="btn-submit" color="primary" onClick={() => this.toggle()}>Submit</Button>{' '}
+                        <Button className="btn-cancel" color="secondary" onClick={() => this.toggle()}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
             </div >
         )
     }
-
 }
 
 const mapStateToProps = state => {
